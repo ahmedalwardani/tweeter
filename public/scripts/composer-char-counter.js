57b83a23504d756ec3b80064c1bf94676ef84ae1
Ahmed Alwardani <aalwa049@gmail.com>
2020-03-24 17:02:39 -0400

    completing Tweeter's home page styling

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+  $("#tweet-text").on("input", function() {
+    const MAX = 140;
+
+    const counterEl = $(this).siblings(".counter");
+    const remaining = MAX - $(this).val().length;
+
+    if (remaining <= 0) {
+      counterEl.css("color", "red");
+    }
+    counterEl.val(remaining);
+    
+    if (remaining > 0) {
+      counterEl.css("color", "#545149");
+    }
+  });
+});
